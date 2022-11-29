a16a4d88c0f92cf03874e4eef8fc3894d5cc8b14
Ben Kates <benekates@gmail.com>
2022-11-29 18:12:34 -0500

    grammar changes

--- yt-history/src/WRITING.js
+++ yt-history/src/WRITING.js
@@ -7,12 +7,12 @@ const WRITING = [
   {
     video_url: "https://www.youtube.com/watch?v=hC8CH0Z3L54",
     ref: "FKJ Tadow",
-    text: `I first saw this video when it came out in 2017, and now it has garnered close to half a BILLION views. This video was an inspiration for me to get back into playing the saxophone post-college after a five-year hiatus. It also inspired me to explore live looping and using simple drum beats to build a track on the spot! Masego triggering his vocal samples while FKJ takes lead on the arrangement stuns me every time I watch. I'm glad this video has spread to so many.`,
+    text: `I first saw this video when it came out in 2017, and now it has garnered close to half a BILLION views. This video was an inspiration for me to get back into playing the saxophone post-college after a five-year hiatus. It also inspired me to explore live looping and using simple drum beats to build a track on the spot! Masego triggering his vocal samples while FKJ takes the lead on the arrangement, stuns me every time I watch. I'm glad this video has spread to so many.`,
   },
   {
     video_url: "https://www.youtube.com/watch?v=fQkC6AkfPmo",
     ref: "MNDSGN Against the clock",
-    text: `I can say with 100% confidence that this video made me realize producing music was not as hard as I had made it out to be in my head. The Against the Clock challenge of "make something in 10 minutes" breeds the most amazing results from the most amazing producers. Seeing their process leads to tons of ideas on how I can start layering my own instruments with a "set it and forget it" mentality. Fellow NJ-native MNDSGN's swung/drunk rhythm feel and instrument stacking with his many synthesizers creates a heavenly vibe that I love so much. Also, you've gotta love seeing his amazing little studio!`,
+    text: `I can say with 100% confidence that this video made me realize producing music was not as hard as I had made it out to be in my head. The Against the Clock challenge of "make something in 10 minutes" breeds the most amazing results from the most amazing producers. Seeing their process leads to tons of ideas on how I can start layering my own instruments with a "set it and forget it" mentality. Fellow NJ-native MNDSGN's swung/drunk rhythm-feel and instrument stacking with his many synthesizers create a heavenly vibe that I love so much. Also, you've gotta love seeing his amazing little studio!`,
   },
   {
     video_url: "https://www.youtube.com/watch?v=4CGBfbB4g0Y",
@@ -22,7 +22,7 @@ const WRITING = [
   {
     video_url: "https://www.youtube.com/watch?v=QrR_gm6RqCo",
     ref: "Mac Tiny Desk",
-    text: `I'll never forget my coworker saying "Mac Miller?" in the fall of 2018 on the day of his passing. I thought "I don't think she knows who that is, I bet something bad happened..." and unfortunately I was right. I grew up with Mac rapping about partying, having fun, feeling sadness, and generally being in touch with what's going on inside. It is poetic that this was one of his last recorded performances. I love seeing the delight on his face when he brings out a string quartet to accompany him on "2009". This video hits hard every time it comes on.`,
+    text: `I'll never forget my older coworker saying "Mac Miller?" in the fall of 2018 on the day of his passing. I thought "I don't think she knows who that is, I bet something bad happened..." and unfortunately I was right. I grew up with Mac rapping about partying, having fun, feeling sad, and generally being in touch with what's going on inside. It is poetic that this was one of his last recorded performances. I love seeing the delight on his face when he brings out a string quartet to accompany him on "2009". This video hits hard every time it comes on.`,
   },
   {
     video_url: "https://www.youtube.com/watch?v=PBKa-AAy_vo",
@@ -32,17 +32,17 @@ const WRITING = [
   {
     video_url: "https://www.youtube.com/watch?v=rv4wf7bzfFE",
     ref: "Vulfpeck MSG",
-    text: `Ugh, what can I say about Vulpeck? Greatest modern funk band? Greatest bassist alive (Joe Dart)?? There are so many Vulfpeck in-home/studio performance videos that have gone viral online, but seeing them bring that same cozy vibe to the big stage is a true joy. Vulfpeck is a collective at this point so the rotation of members and friends brings a diverse set of musical talents that go on and on and on. Watch out for the "arena meditation" at 20 minutes in, it's epic. Fun fact I discovered while writing this: Scary Pockets guitarist Ryan Lerman was the cameraman for this performance and even jumps in for Baby I Don't Know Oh Oh!`,
+    text: `Ugh, what can I say about Vulpeck? Greatest modern funk band? Greatest bassist alive (Joe Dart)?? There are so many Vulfpeck in-home/studio performance videos that have gone viral online, but seeing them bring that same cozy vibe to the big stage is a true joy. Vulfpeck is a collective at this point, so the rotation of members and friends brings a diverse set of musical talents that go on and on and on. Watch out for the "arena meditation" at 20 minutes in, it's epic. Fun fact I discovered while writing this: Scary Pockets guitarist Ryan Lerman was the cameraman for this performance and even jumped in for Baby I Don't Know Oh Oh!`,
   },
   {
     video_url: "https://www.youtube.com/watch?v=bJl-avjRgzU",
     ref: "Parcels COLORS",
-    text: `I believe it was this video in which I discovered one of my favorite bands of all time, Parcels. These Australian dudes are the epitome of tight. Funk has to be really in sync and these guys are beyond that. Based on the interviews I've seen, it seems like they lived together for a few years and would rehearse all day every day. Well, now they're blowing up in a huge way with critical acclaim, a sold-out tour, and even a track collaboration with Daft Punk in 2019 on Overnight.`,
+    text: `I believe it was this video in which I discovered one of my favorite bands of all time, Parcels. These Australian dudes are the epitome of tight. Funk has to be really in sync and these guys are beyond that. Based on interviews I've seen, it seems like they lived together for a few years and would rehearse all day every day. Well, now they're blowing up in a huge way with critical acclaim, a sold-out tour, and even a track collaboration with Daft Punk in 2019 on Overnight.`,
   },
   {
     video_url: "https://www.youtube.com/watch?v=aAFbvMUD5SU",
     ref: "Tom Misch Soulection Studio",
-    text: `Can you tell I love Tom Misch yet? This epic jam ebbs and flows in a way that I think we'd only see live. There is so much build-up to the middle section of the video where everything gets huge with the ambient synths. Tobie Tripp kills it on the violin, one of Tom's original instruments. As a big Soulection Radio fan, I loved seeing one of my favorite artists featured by a radio show I have been listening to for over 8 years.`,
+    text: `Can you tell I love Tom Misch yet? This epic jam ebbs and flows in a way that I think we'd only see live. There is so much build-up to the middle section of the video where everything gets huge with the ambient synths. Tobie Tripp kills it on the violin, one of Tom's original instruments. As a big Soulection Radio fan, I loved seeing one of my favorite artists featured on a radio show I have been listening to for over 8 years.`,
   },
   {
     video_url: "https://www.youtube.com/watch?v=xuc9C-C6Ldw",
@@ -52,7 +52,7 @@ const WRITING = [
   {
     video_url: "https://www.youtube.com/watch?v=kOx2rbcfTMs",
     ref: "Stro Rhythm Roulette",
-    text: `Stro Elliot, now one of the members of The Roots, is one of the best finger drummers in the game. In addition to his impeccable sampling and production skills, he brings an amazingly lifelike sound to playing the drums on a beatpad. I highly recommend the video titled "Doing it for Dilla" where Stro accompanies some musicians hanging out at DJ Jazzy Jeff's PlayList Retreat covering J Dilla tracks. He sounds like he's playing live drums!`,
+    text: `Stro Elliot, now one of the members of The Roots, is one of the best finger drummers in the game. In addition to his impeccable sampling and production skills, he brings an amazingly lifelike sound to playing the drums on a beat pad. I highly recommend the video titled "Doing it for Dilla" where Stro accompanies some musicians hanging out at DJ Jazzy Jeff's PlayList Retreat covering J Dilla tracks. He sounds like he's playing live drums!`,
   },
   {
     video_url: "https://www.youtube.com/watch?v=yLN5TvtdQec",
